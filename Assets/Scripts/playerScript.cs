682c59f8bb7e8ca36fa5a1da4ff2e3a87d5754fc
Richard <ricky201325@gmail.com>
2024-12-17 19:07:51 -0300

    primera parte

--- Assets/Scripts/playerScript.cs
+++ Assets/Scripts/playerScript.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class playerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Rigidbody rb;
+    public float forwardSpeed;
+    public float sideForce;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.AddForce(new Vector3(0, 0, forwardSpeed) * Time.deltaTime);
+        if (Input.GetKey(KeyCode.A)){
+            rb.AddForce(new Vector3(-sideForce, 0, 0) * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D)){
+            rb.AddForce(new Vector3(sideForce, 0, 0) * Time.deltaTime);
+        }
+
     }
 }
